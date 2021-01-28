93398f8bb65d47e89f9f24b3b8334b3929539b92
Asllan Alija <aalija8157@bths.edu>
2021-01-28 17:39:51 -0500

    add courses to db, course handler for http requests, add csv parser to add courses to db

--- server.go
+++ server.go
@@ -24,6 +24,17 @@ type Student struct {
 	Major_GPA float64         `json:"major_gpa,string,omitempty"`
 }
 
+type Course struct {
+	Course_id int64               `json:"student_id,string,omitempty"`
+	Name string                   `json:"name,omitempty"`
+	Department string             `json:"department,string,omitempty"`
+	Course_number int64            `json:"course_number,string,omitempty"`
+	Prereqs string                `json:"prereqs,string,omitempty"`
+	Requirement_fulfilled string  `json:"requirement_fulfilled,string,omitempty"`
+	Credits int64                 `json:"credits,string,omitempty"`
+	Description string            `json:"major_gpades,omitempty"`
+}
+
 func retrieveStudentData(student_id int) (Student) {
 	var id int64
 	var name string
@@ -42,6 +53,25 @@ func retrieveStudentData(student_id int) (Student) {
 	return s
 }
 
+func retrieveCourseData(course_id int) (Course) {
+	var id int64
+	var name string
+	var dep string
+	var cid int64
+	var prereq string
+	var req_f string
+	var credits int64
+	var desc string
+	qtext := fmt.Sprintf("SELECT DISTINCT * FROM courses WHERE course_id LIKE %d", course_id)
+	rows := database.QueryRow(qtext).Scan(&id, &name, &dep, &cid, &prereq, &req_f, &credits, &desc)
+	if rows != nil {
+		log.Fatal(rows)
+	}
+	c := Course{id, name, dep, cid, prereq, req_f, credits, desc}
+	fmt.Println(c)
+	return c
+}
+
 func transactionHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Println("endpoint hit")
@@ -55,7 +85,19 @@ func transactionHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(s)
 }
+func courseHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
+	var id int
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	c := retrieveCourseData(id)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(c)
+}
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "website.html")
 }
@@ -69,10 +111,11 @@ func main() {
 	database = db
 	r := mux.NewRouter()
 	handler:= cors.Default().Handler(r)
-	r.PathPrefix("/static").Handler(http.StripPrefix("/static/", fs))
+    r.PathPrefix("/static/").Handler(http.StripPrefix("/static/",fs))
 	r.HandleFunc("/", rootHandler)
 	r.HandleFunc("/transaction", transactionHandler)
-	http.Handle("/", r)
+	r.HandleFunc("/courses", courseHandler)
+	http.Handle("/", fs)
 	log.Panic(http.ListenAndServe(":8080", handler))
 }
 
